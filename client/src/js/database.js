0ba66d3a4eac19a9415bae9365c519fcc1d57a98
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-11-17 00:25:15 -0500

    edited logic for database.js and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,39 +12,20 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
-  console.log("PUT to the database");
-
-  // creates a connection to the databse and version to use
+export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
-
-  // creates a new transactions and specify the database and data priveleges
-  const tx = jateDb.transaction("jate", "readwrite");
-
-  // open up the desired object store
+  const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-
-  // use .put() method to store and replaces any item with the same key
-  const request = store.put({ id: id, value: content });
-
-  // get confirmation of the request
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log("data saved to the database", result);
+  console.log("Data saved!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log("GET from the database");
-
-  const contactDb = await openDB("jate", 1);
-  const tx = contactDb.transaction("jate", "readonly");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
-
-  // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
   return result;
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,6 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-
 window.addEventListener("beforeinstallprompt", (event) => {
   // Store the triggered events
   window.deferredPrompt = event;
@@ -11,7 +9,6 @@ window.addEventListener("beforeinstallprompt", (event) => {
   butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   const promptEvent = window.deferredPrompt;
 
